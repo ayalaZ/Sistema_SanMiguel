59fc2525e64c4927622be211d713fdc7950e5614
ayalaZ <zenon2gm1994@gmail.com>
2022-04-26 16:53:03 -0600

    Hacerlo igual que el contrato de cable

--- pages/modulo_cxc/php/contratoInternet.php
+++ pages/modulo_cxc/php/contratoInternet.php
@@ -1,4 +1,7 @@
 <?php
+
+use Spipu\Html2Pdf\Tag\Html\Strong;
+
 require '../../../pdfs/fpdf.php';
 require_once("../../../php/config.php");
 require '../../../numLe/src/NumerosEnLetras.php';
@@ -40,20 +43,18 @@ $pdf->Rect(5, 5, 205, 270, 'D');
 $pdf->SetXY(10, 10);
 //FIN DE MARCO
 
-$pdf->Cell(180, 6, utf8_decode('CONTRATO PARA LA PRESTACION DE SERVICIOS DE INTERNET'), 0, 1, 'C');
-$pdf->Image('../../../images/logo.png', 180, 7, 26, 24);
-$pdf->Ln(2);
-$pdf->Cell(52, 2, 'Numero de contrato: ', 0, 0, 'L');
+$pdf->Cell(180, 6, utf8_decode('CONTRATO DE PRESTACION DE SERVICIO DE INTERNET'), 0, 1, 'C');
+$pdf->Image('../../../images/logo.png', 10, 7, 21, 19);
+$pdf->Ln(10);
 $pdf->SetFont('Courier', 'B', 8);
-$pdf->SetTextColor(194, 8, 8);
-$pdf->Cell(18, 2, utf8_decode($prefijocontrato . $result_NC), 0, 1, 'L');
+$pdf->Cell(35, 2, 'Numero de contrato: ', 0, 0, 'L');
+$pdf->Cell(18, 2, utf8_decode($prefijocontrato . $result_NC), 0, 0, 'L');
+$pdf->Cell(50, 2, utf8_decode('CÓDIGO: '), 0, 0, 'R');
+$pdf->Cell(10, 2, $codigo, 0, 1, 'R');
 $pdf->Ln(3);
 $pdf->SetTextColor(0, 0, 0);
 $pdf->SetFont('Courier', 'B', 8);
 $pdf->Cell(100, 2, 'SECCION PRIMERA: DATOS GENERALES DEL CLIENTE', 0, 0, 'L');
-$pdf->Cell(40, 2, utf8_decode('CÓDIGO: '), 0, 0, 'R');
-$pdf->SetTextColor(194, 8, 8);
-$pdf->Cell(10, 2, $codigo, 0, 1, 'R');
 $pdf->SetTextColor(0, 0, 0);
 $pdf->SetFont('Courier', '', 8);
 $pdf->Ln(2);
@@ -66,13 +67,13 @@ function checkbox($pdf, $checked)
         $check = '';
     }
     $pdf->SetFont('ZapfDingbats', '', 15);
-    $pdf->SetTextColor(194, 8, 8);
-    $pdf->Cell(3, 3, $check, 1, 1, 'L');
+
+    $pdf->Cell(3, 3, $check, 1, 0, 'L');
     $pdf->SetTextColor(0, 0, 0);
     $pdf->SetFont('Courier', '', 8);
 }
 checkbox($pdf,TRUE);
-$pdf->Ln(2);
+$pdf->Ln(5);
 $pdf->Cell(28, 2, 'Nombre Completo:', 0, 0, 'L');
 $pdf->Cell(152, 3, $datosCliente['nombre'], 'B', 1, 'L');
 $pdf->Ln(2);
@@ -120,6 +121,80 @@ $pdf->Ln(2);
 $pdf->Cell(180, 3, '', 'B', 1, 'L');
 $pdf->Ln(2);
 $pdf->Cell(45, 2, 'Direccion del instalacion:', 0, 0, 'L');
-$pdf->MultiCell(135,3,strtoupper($datosCliente['direccion']),'B','L',0);
+$pdf->MultiCell(135,3,strtoupper($datosCliente['direccion']),0,'L',0);
+$pdf->Line(55,80,190,80);
+$pdf->Line(10,84,190,84);
+$pdf->Ln(2);
+$pdf->Cell(45, 2, 'Direccion de cobro:', 0, 0, 'L');
+$pdf->MultiCell(135,3,strtoupper($datosCliente['direccion_cobro']),0,'L',0);
+$pdf->Line(43,88,190,88);
+$pdf->Line(10,92,190,92);
+$pdf->Ln(3);
+$pdf->SetFont('Courier', 'B', 8);
+$pdf->Cell(100, 2, 'SECCION SEGUNDA: ESPECIFICACIONES DE LOS SERVICIOS PRESTADOS AL CLIENTE', 0, 1, 'L');
+$pdf->SetFont('Courier', '', 8);
+$pdf->Ln(2);
+$pdf->Cell(50, 2, 'Tipo de contrato:    Nuevo', 0, 0, 'L');
+if ($datosCliente['tipo_de_contrato'] == 'Nuevo') {
+    checkbox($pdf,TRUE); 
+}else{
+    checkbox($pdf,FALSE);
+}
+$pdf->Cell(30, 2, '     Reconexion', 0, 0, 'L');
+if ($datosCliente['tipo_de_contrato'] == 'Reconexion') {
+    checkbox($pdf,TRUE); 
+}else{
+    checkbox($pdf,FALSE);
+}
+$pdf->Cell(30, 2, '     Renovacion', 0, 0, 'L');
+if ($datosCliente['tipo_de_contrato'] == 'Renovacion') {
+    checkbox($pdf,TRUE); 
+}else{
+    checkbox($pdf,FALSE);
+}
+$pdf->Ln(5);
+$pdf->Cell(50, 2, 'Tipo de cliente: Residencial', 0, 0, 'L');
+if ($datosCliente['id_tipo_cliente'] == '0002') {
+    checkbox($pdf,TRUE); 
+}else{
+    checkbox($pdf,FALSE);
+}
+$pdf->Cell(30, 2, '     Pyme', 0, 0, 'L');
+if ($datosCliente['id_tipo_cliente'] == '0004') {
+    checkbox($pdf,TRUE); 
+}else{
+    checkbox($pdf,FALSE);
+}
+$pdf->Cell(30, 2, '     Corporativo', 0, 0, 'L');
+if ($datosCliente['id_tipo_cliente'] == '0003') {
+    checkbox($pdf,TRUE); 
+}else{
+    checkbox($pdf,FALSE);
+}
+$pdf->Ln(5);
+$pdf->Cell(30, 2, 'Tipo de servicio:', 0, 0, 'L');
+$pdf->Cell(40, 3, 'INTERNET', 'B', 0, 'C');
+$pdf->Cell(19, 2, 'Velocidad:', 0, 0, 'L');
+$idVelocidad = $datosCliente['id_velocidad'];
+$velocidad = $mysqli->query("SELECT nombreVelocidad FROM tbl_velocidades WHERE idVelocidad='$idVelocidad'");
+$datosVelocidad = $velocidad->fetch_array();
+$pdf->Cell(40, 3, $datosVelocidad['nombreVelocidad'], 'B', 0, 'C');
+$pdf->Cell(19, 2, 'Tecnologia:', 0, 0, 'L');
+$pdf->Cell(32, 3, $datosCliente['tecnologia'], 'B', 1, 'C');
+$pdf->Ln(2);
+$pdf->Cell(23, 2, 'Mac de modem:', 0, 0, 'L');
+$pdf->Cell(30, 3, $datosCliente['mac_modem'], 'B', 0, 'C');
+$pdf->Cell(28, 2, 'Serial de modem:', 0, 0, 'L');
+$pdf->Cell(30, 3, $datosCliente['serie_modem'], 'B', 0, 'C');
+$pdf->Cell(42, 2, 'Entregado en calidad de:', 0, 0, 'L');
+$pdf->Cell(27, 3, $datosCliente['entrega_calidad'], 'B', 1, 'C');
+$pdf->Ln(3);
+$pdf->SetFont('Courier', 'B', 8);
+$pdf->Cell(100, 2, 'SECCION TERCERA: TERMINOS Y CONDICIONES', 0, 1, 'L');
+$pdf->SetFont('Courier', '', 8);
+$pdf->Ln(2);
+$pdf->MultiCell(180,3,utf8_decode('Los terminos y condiciones para la prestación de Servicio de Internet, por parte de CABLE VISION POR SATELITE SOCIEDAD ANONIMA DE CAPITAL VARIABLE, que se abrevia CABLE SAT S.A. DE C.V. Que en el desarrollo del presente instrumento podrá citarse como "CABLE SAT". Las condiciones particulares, en cuanto a plazo, plan o paquete contratado, tarifas, garantias, especificaciones de equipos para la prestacion del servicio a cada cliente, se encuentra detalladas en este CONTRATO DE SERVICIO que el CLIENTE voluntariamente se suscribe y acepta.'),0,'J',0);
+$pdf->Ln(5);
+$pdf->MultiCell(180,3,utf8_decode('1. CLIENTE. Declaro que recibiré de parte de CABLE SAT el servicio de internet hasta la finalización del plazo acordado; y estoy consciente que el contrato de servicio entra en vigencia a partir de la fecha de suscripción y se renueva automaticamente por plazos iguales, una vez haya transcurrido diez dias despues del vencimiento del contrato.'),0,'J',0);
 mysqli_close($mysqli);
 $pdf->Output();
